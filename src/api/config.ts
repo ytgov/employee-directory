16e41d50e33c980f08c518ff28a37eab8640e28b
Mariazel Hernandez <mariazel@bizont.ca>
2023-03-20 13:02:34 -0700

    Update port

--- src/api/config.ts
+++ src/api/config.ts
@@ -15,6 +15,6 @@ switch (process.env.NODE_ENV) {
 dotenv.config({ path: path });
 console.log(`Loading config from ${process.env.NODE_ENV || 'dev'}`);
 
-export const API_PORT = parseInt(process.env.API_PORT || "8080");
+export const API_PORT = parseInt(process.env.API_PORT || "3000");
 export const FRONTEND_URL = process.env.FRONTEND_URL || "localhost:8080";
 export const NODE_ENV = process.env.NODE_ENV;
