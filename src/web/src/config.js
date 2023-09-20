5058424944eef57929497841572cbe77fb462b63
Mariazel Hernández <mariazelhernandez@Mariazels-MacBook-Pro.local>
2023-09-20 10:23:33 -0700

    Remove comments from the code

--- src/web/src/config.js
+++ src/web/src/config.js
@@ -22,4 +22,3 @@ export const sections = [
 
 export const environment = process.env.NODE_ENV;
 export const apiBaseUrl = (process.env.NODE_ENV == "test" || process.env.NODE_ENV == "production") ? "" : "http://localhost:3000";
-//export const apiBaseUrl = process.env.VUE_APP_API_URL || "https://find-employee.service.yukon.ca";
