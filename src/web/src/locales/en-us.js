1932bb753895de4fc572168832efb5dd4671b9a4
Emanuel Pastrana <emanuel@bizont.ca>
2023-09-29 12:26:34 -0600

    languages added in object

---
+++ src/web/src/locales/en-us.js
@@ -0,0 +1,165 @@
+export const en = {
+  components: {
+    search_bar_header: {
+      title: "Find a government employee",
+      search: {
+        label: "Enter a person's name, position title, email or phone number in the search bar.",
+        input_placeholder: "Keywords"
+      },
+    },
+    employees: {
+      browse_directory_by_department: "Browse the employee directory by department"
+    },
+    feedback_form: {
+      questions: {
+        helpful_message: "Was this page helpful?",
+        how_help_you_message: "How did this page help you?",
+        how_improve_message: "How can we improve this page?",
+      },
+      buttons: {
+        submit_feedback: "Submit feedback",
+      },
+      alerts: {
+        thanks_feedback: "Thanks for taking the time to send us your feedback.",
+        field_is_required: "field is required.",
+        try_again: "Please try again later!"
+      }
+    },
+    department: {
+      labels: {
+        browse_employees_by_divisions: "Browse employees by these divisions",
+        no_results: "There are no results.",
+        view_list: "View a list of all Department of",
+        group_by: "Group by:",
+        see_all_employees: "See all government employees",
+        location: "Location",
+        position: "Position",
+        results: "Results"
+      },
+    },
+    employee_details: {
+      organization: {
+        title: "Organization",
+        department: "Department",
+        division: "Division",
+        branch: "Branch",
+        unit: "Unit",
+      },
+      contact: {
+        title: "Contact",
+        phone_office: "Phone office",
+        email_address: "Email address",
+        fax_office: "Fax office",
+      },
+      position_information: {
+        title: "Position information",
+        manager: "Reports to",
+      },
+      location: {
+        title: "Location",
+        address: "Address",
+        community: "Community",
+        postal_code: "Postal code",
+        mail_code: "Mail code"
+      }
+    },
+    employee_search: {
+      no_results_by_department: {
+        body: {
+          part1: "Your search for",
+          part2: "in the department",
+          part3: "had no results."
+        }
+      },
+      no_results: {
+        body: {
+          part1: "Your search for",
+          part2: "has no results."
+        }
+      },
+      results_by_department: {
+        body: {
+          part1: "Your search for",
+          part2: "in the department",
+          part3: "found",
+          part4: "results."
+        }
+      },
+      results: {
+        body: {
+          part1: "Your search for",
+          part2: "found",
+          part3: "results."
+        }
+      },
+      labels: {
+        group_by: "Group by",
+        see_all: "See all government employees",
+        department: "Department",
+        location: "Location",
+        position: "Position"
+      }
+    },
+    grid: {
+      group_by: "Group by",
+      see_all: "See all government employees",
+      location: "Location",
+      position: "Position",
+      no_results: "There are no results.",
+      results: "Results"
+    },
+    not_found: {
+      page_not_found: "Page non trouvée",
+      page_is_not_here: {
+        title: "Sorry, that page isn't here.",
+        body: "You didn't do anything wrong. We may have moved the page you're looking for somewhere else."
+      },
+      reached_from_inside: {
+        title: "Did you follow a link from here?",
+        body: {
+          part1: "If you reached this page from another part of Yukon.ca, ",
+          part2: "let us know ",
+          part3: "so we can correct our mistake."
+        }
+      },
+      reached_from_outside: {
+        title: "Did you follow a link from another site?",
+        body: {
+          part1: "Links from other sites can sometimes be old or misspelled. ",
+          part2: "Let us know ",
+          part3: "where you came from and we can try to contact the other site to fix the problem."
+        }
+      },
+      address_wrong_message: {
+        title: "Did you type the URL?",
+        body: "You may have typed the address (URL) wrong. Check to make sure you've got the exact right spelling or capitalization."
+      }
+    },
+  },
+  breadcrumbs: {
+    home: "Yukon.ca home",
+    find_a_government_employee: "Find a government employee",
+    department: "Department",
+    search: "Search",
+    division: "Division",
+    branch: "Branch",
+    username: "Username",
+    find_employee: "Find Employee",
+    page_not_found: "Page not found"
+  },
+  footer: {
+    sections: {
+      government: "Government of Yukon",
+      copyright: "Copyright",
+      disclaimer: "Disclaimer",
+      privacy: "Privacy statement"
+    }
+  },
+  yukon_urls: {
+    home: "https://yukon.ca/en",
+    copyright: "https://yukon.ca/en/copyright",
+    disclaimer: "https://yukon.ca/en/disclaimer",
+    privacy_statement: "https://yukon.ca/en/privacy-statement",
+    contact_government: "https://yukon.ca/en/your-government/contact-and-follow-government/contact-government"
+  }
+};
